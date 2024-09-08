b8ac1fcaf149d79f2a2b7d65ad99d3604cdb52b1
roni836 <ronitsaha836@gmail.com>
2024-09-08 08:26:26 +0530

    dashboard work done

--- resources/views/admin/adminBase.blade.php
+++ resources/views/admin/adminBase.blade.php
@@ -5,9 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-        eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template
-    </title>
+    <title>Bihar shop | Admin - @yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="{{ asset('assets/assets_admin/js/index.js') }}"></script>
     <link rel="icon" href="favicon.ico">
@@ -59,7 +57,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
                             <!-- Menu Item Dashboard -->
                             <li>
                                 <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
-                                    href="#"
+                                    href="{{url('admin')}}"
                                     @click.prevent="selected = (selected === 'Dashboard' ? '':'Dashboard')"
                                     :class="{
                                         'bg-graydark dark:bg-meta-4': (selected === 'Dashboard') || (
@@ -83,8 +81,61 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
 
                                     Dashboard
 
-                                    <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                    {{-- <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
                                         :class="{ 'rotate-180': (selected === 'Dashboard') }" width="20"
+                                        height="20" viewBox="0 0 20 20" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                                            fill="" />
+                                    </svg> --}}
+                                </a>
+
+                                <!-- Dropdown Menu Start -->
+                                {{-- <div class="translate transform overflow-hidden"
+                                    :class="(selected === 'Dashboard') ? 'block' : 'hidden'">
+                                    <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                                        <li>
+                                            <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                                href="index.html"
+                                                :class="page === 'ecommerce' && '!text-white'">eCommerce
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div> --}}
+                                <!-- Dropdown Menu End -->
+                            </li>
+
+                            <li>
+                                <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                    href="javascript:void(0)" @click.prevent="selected = (selected === 'Categories' ? '':'Categories')"
+                                    :class="{
+                                        'bg-graydark dark:bg-meta-4': (selected === 'Categories') || (
+                                            page === 'addCategory' || page === 'manageCategory')
+                                    }">
+                                    <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18"
+                                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M1.43425 7.5093H2.278C2.44675 7.5093 2.55925 7.3968 2.58737 7.31243L2.98112 6.32805H5.90612L6.27175 7.31243C6.328 7.48118 6.46862 7.5093 6.58112 7.5093H7.453C7.76237 7.48118 7.87487 7.25618 7.76237 7.03118L5.428 1.4343C5.37175 1.26555 5.3155 1.23743 5.14675 1.23743H3.88112C3.76862 1.23743 3.59987 1.29368 3.57175 1.4343L1.153 7.08743C1.0405 7.2843 1.20925 7.5093 1.43425 7.5093ZM4.47175 2.98118L5.3155 5.17493H3.59987L4.47175 2.98118Z"
+                                            fill="" />
+                                        <path
+                                            d="M10.1249 2.5031H16.8749C17.2124 2.5031 17.5218 2.22185 17.5218 1.85623C17.5218 1.4906 17.2405 1.20935 16.8749 1.20935H10.1249C9.7874 1.20935 9.47803 1.4906 9.47803 1.85623C9.47803 2.22185 9.75928 2.5031 10.1249 2.5031Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.8749 6.21558H10.1249C9.7874 6.21558 9.47803 6.49683 9.47803 6.86245C9.47803 7.22808 9.75928 7.50933 10.1249 7.50933H16.8749C17.2124 7.50933 17.5218 7.22808 17.5218 6.86245C17.5218 6.49683 17.2124 6.21558 16.8749 6.21558Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.875 11.1656H1.77187C1.43438 11.1656 1.125 11.4469 1.125 11.8125C1.125 12.1781 1.40625 12.4594 1.77187 12.4594H16.875C17.2125 12.4594 17.5219 12.1781 17.5219 11.8125C17.5219 11.4469 17.2125 11.1656 16.875 11.1656Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.875 16.1156H1.77187C1.43438 16.1156 1.125 16.3969 1.125 16.7625C1.125 17.1281 1.40625 17.4094 1.77187 17.4094H16.875C17.2125 17.4094 17.5219 17.1281 17.5219 16.7625C17.5219 16.3969 17.2125 16.1156 16.875 16.1156Z"
+                                            fill="white" />
+                                    </svg>
+
+                                    Categories
+
+                                    <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                        :class="{ 'rotate-180': (selected === 'Categories') }" width="20"
                                         height="20" viewBox="0 0 20 20" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd"
@@ -95,13 +146,74 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
 
                                 <!-- Dropdown Menu Start -->
                                 <div class="translate transform overflow-hidden"
-                                    :class="(selected === 'Dashboard') ? 'block' : 'hidden'">
+                                    :class="(selected === 'Categories') ? 'block' : 'hidden'">
                                     <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
                                         <li>
                                             <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
-                                                href="index.html"
-                                                :class="page === 'ecommerce' && '!text-white'">eCommerce
-                                            </a>
+                                                href="{{url('admin/add-category')}}"
+                                                :class="page === 'addCategory' && '!text-white'">Add Category</a>
+                                        </li>
+                                        <li>
+                                            <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                                href="{{url('admin/manage-category')}}"
+                                                :class="page === 'manageCategory' && '!text-white'">Manage Categories</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- Dropdown Menu End -->
+                            </li>
+
+                            <li>
+                                <a class="group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4"
+                                    href="javascript:void(0)" @click.prevent="selected = (selected === 'Products' ? '':'Products')"
+                                    :class="{
+                                        'bg-graydark dark:bg-meta-4': (selected === 'Products') || (
+                                            page === 'addProduct' || page === 'manageProduct')
+                                    }">
+                                    <svg class="fill-current" width="18" height="18" viewBox="0 0 18 18"
+                                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M1.43425 7.5093H2.278C2.44675 7.5093 2.55925 7.3968 2.58737 7.31243L2.98112 6.32805H5.90612L6.27175 7.31243C6.328 7.48118 6.46862 7.5093 6.58112 7.5093H7.453C7.76237 7.48118 7.87487 7.25618 7.76237 7.03118L5.428 1.4343C5.37175 1.26555 5.3155 1.23743 5.14675 1.23743H3.88112C3.76862 1.23743 3.59987 1.29368 3.57175 1.4343L1.153 7.08743C1.0405 7.2843 1.20925 7.5093 1.43425 7.5093ZM4.47175 2.98118L5.3155 5.17493H3.59987L4.47175 2.98118Z"
+                                            fill="" />
+                                        <path
+                                            d="M10.1249 2.5031H16.8749C17.2124 2.5031 17.5218 2.22185 17.5218 1.85623C17.5218 1.4906 17.2405 1.20935 16.8749 1.20935H10.1249C9.7874 1.20935 9.47803 1.4906 9.47803 1.85623C9.47803 2.22185 9.75928 2.5031 10.1249 2.5031Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.8749 6.21558H10.1249C9.7874 6.21558 9.47803 6.49683 9.47803 6.86245C9.47803 7.22808 9.75928 7.50933 10.1249 7.50933H16.8749C17.2124 7.50933 17.5218 7.22808 17.5218 6.86245C17.5218 6.49683 17.2124 6.21558 16.8749 6.21558Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.875 11.1656H1.77187C1.43438 11.1656 1.125 11.4469 1.125 11.8125C1.125 12.1781 1.40625 12.4594 1.77187 12.4594H16.875C17.2125 12.4594 17.5219 12.1781 17.5219 11.8125C17.5219 11.4469 17.2125 11.1656 16.875 11.1656Z"
+                                            fill="" />
+                                        <path
+                                            d="M16.875 16.1156H1.77187C1.43438 16.1156 1.125 16.3969 1.125 16.7625C1.125 17.1281 1.40625 17.4094 1.77187 17.4094H16.875C17.2125 17.4094 17.5219 17.1281 17.5219 16.7625C17.5219 16.3969 17.2125 16.1156 16.875 16.1156Z"
+                                            fill="white" />
+                                    </svg>
+
+                                    Products
+
+                                    <svg class="absolute right-4 top-1/2 -translate-y-1/2 fill-current"
+                                        :class="{ 'rotate-180': (selected === 'Products') }" width="20"
+                                        height="20" viewBox="0 0 20 20" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                                            fill="" />
+                                    </svg>
+                                </a>
+
+                                <!-- Dropdown Menu Start -->
+                                <div class="translate transform overflow-hidden"
+                                    :class="(selected === 'Products') ? 'block' : 'hidden'">
+                                    <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+                                        <li>
+                                            <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                                href="{{url('admin/add-product')}}"
+                                                :class="page === 'addProduct' && '!text-white'">Add Product</a>
+                                        </li>
+                                        <li>
+                                            <a class="group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white"
+                                                href="{{url('admin/manage-product')}}"
+                                                :class="page === 'manageProduct' && '!text-white'">Manage Products</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -844,8 +956,7 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
             </header>
 
 
-            @section('content')
-
+            @yield('content')
             @show
 
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,20 @@ Route::get('/admin', function () {
     return view('admin.dashboard');
 });
 
+Route::get('/admin/add-category', function () {
+    return view('admin.insertCategory');
+});
+
+Route::get('/admin/manage-category', function () {
+    return view('admin.manageCategory');
+});
+
+Route::get('/admin/add-product', function () {
+    return view('admin.insertProduct');
+});
+
+Route::get('/admin/manage-product', function () {
+    return view('admin.manageProduct');
+});
+
 // Route::view('/admin', 'admin.dashboard');
