466060d478c07e1a23fd468b191f4a462bf6b942
Saumy Anand <samcool3203@gmail.com>
2024-09-08 07:55:17 +0530

    navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -12,12 +13,15 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+
 
 
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
+Route::get('/',[PublicController::class,"index"])->name("index");
+
 
 // Route::view('/admin', 'admin.dashboard');
+require __DIR__.'/auth.php';
