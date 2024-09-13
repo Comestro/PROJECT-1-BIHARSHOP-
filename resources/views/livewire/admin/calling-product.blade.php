67ab99f62e3b6e8796887ce1f59a2eb07152462f
KomalKumari17 <komalydv2760@gmail.com>
2024-09-13 10:26:03 +0530

    search of category and product

--- resources/views/livewire/admin/calling-product.blade.php
+++ resources/views/livewire/admin/calling-product.blade.php
@@ -584,7 +584,27 @@
     </div> --}}
 
 
-
+    <div class="flex flex-1 justify-between">
+        <div>        
+           <h1 class="text-2xl font-bold mb-4">Manage Product</h1>
+         </div>        
+         <div class="">
+             <div class="relative flex flex-1">
+                 <input type="search" 
+                        class="border w-[200px] pl-8 pr-2 py-1 rounded-2xl border-none ring-1 ring-gray-300 focus:ring-gray-400 focus:ring-2" 
+                        placeholder="search here.." 
+                        wire:model.live='searchTerm' />
+                 <svg xmlns="http://www.w3.org/2000/svg" 
+                      class="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" 
+                      width="20" 
+                      height="20" 
+                      viewBox="0 0 48 48">
+                   <path d="M 20.5 6 C 12.509634 6 6 12.50964 6 20.5 C 6 28.49036 12.509634 35 20.5 35 C 23.956359 35 27.133709 33.779044 29.628906 31.75 L 39.439453 41.560547 A 1.50015 1.50015 0 1 0 41.560547 39.439453 L 31.75 29.628906 C 33.779044 27.133709 35 23.956357 35 20.5 C 35 12.50964 28.490366 6 20.5 6 z M 20.5 9 C 26.869047 9 32 14.130957 32 20.5 C 32 23.602612 30.776198 26.405717 28.791016 28.470703 A 1.50015 1.50015 0 0 0 28.470703 28.791016 C 26.405717 30.776199 23.602614 32 20.5 32 C 14.130953 32 9 26.869043 9 20.5 C 9 14.130957 14.130953 9 20.5 9 z"></path>
+                 </svg>
+               </div>
+               
+         </div>
+     </div>
     <div>
         @if (session()->has('success'))
             <div class="bg-green-500 text-white p-4 rounded mb-4">
@@ -633,7 +653,7 @@
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                             <a href="{{ route('product.edit', $product->slug) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                            <button type="button" wire:click="delete({{ $product->id }})" class="text-red-600 hover:text-red-900">Delete</button>
+                            <button type="button" wire:click="confirmDelete({{ $product->id }})" class="text-red-600 hover:text-red-900">Delete</button>
                         </td>
                     </tr>
                 @endforeach
@@ -642,3 +662,5 @@
     </div>
 
 </div>
+
+
