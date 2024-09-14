84c605ae576af8092bcff7453d81131e3ae50ef6
khobabe <khobabe007@gmail.com>
2024-09-14 19:36:41 +0530

    Added googlel-authentication to the application

--- config/services.php
+++ config/services.php
@@ -28,6 +28,12 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
