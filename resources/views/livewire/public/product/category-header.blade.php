41c5dda5e33edba82e37d0597334d9bd5fe38540
Saumy Anand <samcool3203@gmail.com>
2024-11-04 12:50:55 +0530

    design change in gallery page

--- resources/views/livewire/public/product/category-header.blade.php
+++ resources/views/livewire/public/product/category-header.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
     @forelse ($categories as $category)
         <a wire:navigate href="{{ route('filter', ['cat_slug' => $category->cat_slug, 'cat_id' => $category->id]) }}">
-            <div class="group relative h-32 bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300">
+            <div class="group relative h-32      bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-shadow duration-300">
                 <img src="{{ $category->image ? asset('storage/image/category/' . $category->image) : asset('path/to/default-image.jpg') }}"
                     alt="{{ $category->name }}"
                     class="w-full h-36 object-cover object-center group-hover:scale-110 transition-transform duration-300" />
