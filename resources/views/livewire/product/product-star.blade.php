67754c593e100335218aedb013f9a0f5a2401c2f
rahulxdev0 <rk854327@gmail.com>
2024-11-05 10:42:18 +0530

    some design changes in product view page and home page

--- resources/views/livewire/product/product-star.blade.php
+++ resources/views/livewire/product/product-star.blade.php
@@ -5,16 +5,16 @@
            <div class="flex flex-col md:flex items-center">
            <div class="flex items-center">
                 @for ($i = 1; $i <= 5; $i++)
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 fill-current {{ $i <= $averageRating ? 'text-yellow-500' : 'text-gray-300' }}"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-4 fill-current {{ $i <= $averageRating ? 'text-yellow-500' : 'text-gray-300' }}"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M12 17.27L18.18 21 16.54 13.97 22 9.24l-8.24-.69L12 2 10.24 8.55 2 9.24l5.46 4.73L5.82 21z" />
                     </svg>
                 @endfor
-                <span class="text-normal font-semibold text-zinc-800 ">{{ number_format($averageRating, 1) }}</span>
+                <span class="text-sm font-semibold text-zinc-800 ml-1">{{ number_format($averageRating, 1) }}</span>
 
             </div>
            </div>
-            <span class="text-gray-600">({{$totalReviews}} {{ $totalReviews == 1? "review":"reviews" }})</span>
+            <span class="text-gray-600 text-xs sm:text-sm flex truncate">| {{$totalReviews}} {{ $totalReviews == 1? "review":"reviews" }}</span>
         </div>
     </div>
 
