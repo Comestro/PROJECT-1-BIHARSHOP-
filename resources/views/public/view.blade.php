e184d4966c2521acdbea69bad26b727de4fea54a
Comestro <comestrotechlabs@gmail.com>
2024-11-14 17:32:23 +0530

    minor

--- resources/views/public/view.blade.php
+++ resources/views/public/view.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
     <!-- Product Section -->
-    <section class="max-w-6xl mx-auto p-5 mt-10">
+    <section class="mx-auto p-5 mt-10">
         <!-- Grid for Images and Info -->
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
             <!-- Left Section: Images -->
